fad046ead8c128dbd3f7bf41abcbc3d40b1e2cb1
CadeV4 <99850501+CadeV4@users.noreply.github.com>
2024-04-24 23:06:30 -0500

    Added shield

--- Flow Runner/Assets/Scripts/PlayerScripts/RockKill.cs
+++ Flow Runner/Assets/Scripts/PlayerScripts/RockKill.cs
@@ -3,18 +3,38 @@ using UnityEngine.SceneManagement;
 
 public class RockCollision : MonoBehaviour
 {
+    public GameObject Player;
+    private PlayerMovement script;
+    private void Start()
+    {
+        script = Player.GetComponent<PlayerMovement>();
+    }
+
     public void OnTriggerEnter2D(Collider2D other)
     {
+        bool isShielded = script.shielded;
+
         if (!PauseManager.isPaused && other.CompareTag("Player"))
         {
-            // Handle player death here (e.g., restart the level, reduce player health, etc.)
-            Debug.Log("Player hit by rock!");
-            SceneManager.LoadSceneAsync("Title Screen");
+            if (isShielded)
+            {
 
-            // Destroy the rock after hitting the player
-            Destroy(gameObject);
+                // Handle player death here (e.g., restart the level, reduce player health, etc.)
+                Debug.Log("Player hit by rock!");
+                SceneManager.LoadSceneAsync("Title Screen");
+
+                // Destroy the rock after hitting the player
+                // Destroy the rock after hitting the player
+                Destroy(gameObject);
+            }
+            else
+                Destroy(gameObject);
         }
         else if (!PauseManager.isPaused && other.CompareTag("Ground")) // collision with platforms
+        {
+            Destroy(gameObject);
+        }
+        else if (!PauseManager.isPaused && other.CompareTag("Shield")) // collision with platforms
         {
             Destroy(gameObject);
         }
