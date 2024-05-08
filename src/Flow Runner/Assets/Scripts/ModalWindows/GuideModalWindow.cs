5c56d436461e9c6ef51757e2d63d95ed513cf519
Zach Landquist <landquistz@uwplatt.edu>
2024-05-07 21:25:59 -0500

    Minimum working guide

--- src/Flow Runner/Assets/Scripts/ModalWindows/GuideModalWindow.cs
+++ src/Flow Runner/Assets/Scripts/ModalWindows/GuideModalWindow.cs
@@ -28,25 +28,17 @@ public class GuideModalWindow : ModalWindow<GuideModalWindow>
     [Serializable]
     public class GuideObjectsPage1 : IGuidePage
     {
-        // kase, create copies for each page
-        // we can iterate over these things easy
-        // so like if i click the next button, then iterate over page1 and hide and then do the same for page2 but show
         public GameObject pageParent;
         public TMP_Text bodyText;
 
-        // kase, these are just examples for how to show the settings stuff
         public TMP_Text nameText;
-        public TMP_Text volumeText;
         public TMP_Text[] keyTexts;
 
-        // kase, the HidePageObjects(pageParent); is required, but if you needed to run any other functions then put them after the hide
         public void Hide()
         {
             HidePageObjects(pageParent);
         }
 
-        // kase, the ShowPageObjects(pageParent); is required, but if you needed to run any other functions then put them before the show
-        // like how i am setting the vars by getting the settings instance
         public void Show()
         {
             SetFromSettings();
@@ -59,13 +51,6 @@ public class GuideModalWindow : ModalWindow<GuideModalWindow>
             if (nameText != null)
             {
                 nameText.text = SettingsManager.Instance.PlayerName;
-            }
-
-            // Set volume text
-            if (volumeText != null)
-            {
-                int volumePercentage = Mathf.RoundToInt(SettingsManager.Instance.Volume);
-                volumeText.text = volumePercentage.ToString() + "%";
             }
 
             // Set key texts
@@ -81,13 +66,9 @@ public class GuideModalWindow : ModalWindow<GuideModalWindow>
     [Serializable]
     public class GuideObjectsPage2 : IGuidePage
     {
-        // kase, this is a blank one to setup whatever
         public GameObject pageParent;
         public TMP_Text bodyText;
 
-        // kase, setup other ui items here to hook in from the editor
-
-        // kase, modify these if you need to add any other functions
         public void Hide()
         {
             HidePageObjects(pageParent);
@@ -103,7 +84,6 @@ public class GuideModalWindow : ModalWindow<GuideModalWindow>
     [SerializeField] private GuideObjectsPage1 guideObjectsPage1;
     [SerializeField] private GuideObjectsPage2 guideObjectsPage2;
     //[SerializeField] private GuideObjectsPage3 guideObjectsPage3;
-    // kase, etc.
     [SerializeField] private Button prevButton;
     [SerializeField] private Button nextButton;
     #pragma warning restore IDE0044 // Add readonly modifier
@@ -143,7 +123,6 @@ public class GuideModalWindow : ModalWindow<GuideModalWindow>
             new(guideObjectsPage1),
             new(guideObjectsPage2),
             //new(guideObjectsPage3),
-            // kase, etc.
         };
     }
 
